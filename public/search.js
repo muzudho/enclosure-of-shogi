a666e362fa1cf7e183124cebdd9a240975e81b26
むずでょ <muzudho1@gmail.com>
2020-08-01 17:36:34 +0900

    WIP. Search.

--- public/search.js
+++ public/search.js
@@ -1,4 +1,91 @@
-function create_board(input) {
+let nodes_count;
+function search(input) {
+    nodes_count = 0;
+    let board = createBoard(input);
+    let checkBoard = createFalseBoard(input);
+    let curSq = find('K', board);
+    node(curSq, board, checkBoard);
+}
+
+function node(curSq, board, checkBoard) {
+    checkBoard[curSq] = true;
+    let ways = genMove(curSq, board, checkBoard);
+    alert(`ways.length=${ways.length}`);
+    if (ways.length === 0) {
+        // TODO Evaluation.
+        nodes_count++;
+    }
+    for (nextSq of ways) {
+        switch (board[nextSq]) {
+            case 'G':
+                node(nextSq, board, checkBoard);
+                break;
+            case 'S':
+                node(nextSq, board, checkBoard);
+                break;
+            default:
+                break;
+        }
+    }
+    checkBoard[curSq] = false;
+}
+
+/**
+ * Generation move.
+ */
+function genMove(sq, board, checkBoard) {
+    let ways = [];
+
+    switch (board[sq]) {
+        case 'K':
+            pushWay(sq - 10, ways, board, checkBoard);
+            pushWay(sq - 11, ways, board, checkBoard);
+            pushWay(sq - 1, ways, board, checkBoard);
+            pushWay(sq + 9, ways, board, checkBoard);
+            pushWay(sq + 10, ways, board, checkBoard);
+            pushWay(sq + 11, ways, board, checkBoard);
+            pushWay(sq + 1, ways, board, checkBoard);
+            pushWay(sq - 9, ways, board, checkBoard);
+            break;
+        case 'G':
+            pushWay(sq - 10, ways, board, checkBoard);
+            pushWay(sq - 11, ways, board, checkBoard);
+            pushWay(sq - 1, ways, board, checkBoard);
+            pushWay(sq + 9, ways, board, checkBoard);
+            pushWay(sq + 10, ways, board, checkBoard);
+            pushWay(sq + 1, ways, board, checkBoard);
+            break;
+        case 'S':
+            pushWay(sq - 11, ways, board, checkBoard);
+            pushWay(sq - 1, ways, board, checkBoard);
+            pushWay(sq + 9, ways, board, checkBoard);
+            pushWay(sq + 11, ways, board, checkBoard);
+            pushWay(sq - 9, ways, board, checkBoard);
+            break;
+        default:
+            break;
+    }
+    return ways;
+}
+function pushWay(nextSq, ways, board, checkBoard) {
+    if (!checkBoard[nextSq] && (board[nextSq] === 'G' || board[nextSq] === 'S')) {
+        ways.push(nextSq);
+    }
+}
+function find(piece, board) {
+    for (const [i, sq] of board.entries()) {
+        /*
+        if (70 < i) {
+            alert(`i=${i} sq=${sq}`);
+        }
+        */
+        if (sq === piece) {
+            return i;
+        }
+    }
+}
+
+function createBoard(input) {
     board = [];
 
     for (rank = 1; rank < 10; rank++) {
@@ -28,15 +115,14 @@ function create_board(input) {
     return board;
 }
 
-function find(piece, board) {
-    for (const [i, sq] of board.entries()) {
-        /*
-        if (70 < i) {
-            alert(`i=${i} sq=${sq}`);
-        }
-        */
-        if (sq === piece) {
-            return i;
+function createFalseBoard() {
+    board = [];
+
+    for (rank = 1; rank < 10; rank++) {
+        for (file = 9; 0 < file; file--) {
+            board[file * 10 + rank] = false;
         }
     }
+
+    return board;
 }
