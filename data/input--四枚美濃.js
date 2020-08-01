c2b335824342d8ab62f2de9c49c57f797933ca65
むずでょ <muzudho1@gmail.com>
2020-08-01 14:37:44 +0900

    tensyukaku

--- data/input--四枚美濃.js
+++ data/input--四枚美濃.js
@@ -1,30 +1,33 @@
 function get_data() {
     return {
         "enclosureName": [
-            "左美濃囲い", "天守閣美濃", "Hidari Mino Gakoi", "Tensyukaku Mino"
+            "左美濃囲い", "四枚美濃", "Hidari Mino Gakoi", "Yonmai Mino"
         ],
         "board": [
             ["sq96", "pc_0"],
             ["sq86", "pc_0"],
             ["sq76", "pc_0"],
+            ["sq66", "pc_0"],
             ["sq56", "pc_0"],
 
             ["sq87", "pc_k"],
-            ["sq67", "pc_0"],
+            ["sq77", "pc_s"],
+            ["sq67", "pc_g"],
 
-            ["sq88", "pc_0"],
             ["sq78", "pc_s"],
-            ["sq58", "pc_g"],
 
             ["sq99", "pc_0"],
             ["sq89", "pc_0"],
+            ["sq79", "pc_0"],
             ["sq69", "pc_g"]
         ],
         "arrow": [
             ["d87", "k48"],
             ["d78", "a4884"],
-            ["d68", "a62"],
-            ["c58", "a1"]
+            ["h77", "a73"],
+            ["c77", "a1"],
+            ["d77", "a62"],
+            ["c67", "a1"]
         ]
     };
 }
