84ec409770ba8b7039dc616cecf73b65f3c17b05
むずでょ <muzudho1@gmail.com>
2020-08-01 14:14:00 +0900

    半自動化☆（＾～＾）

--- public/input.js
+++ public/input.js
@@ -1,25 +1,27 @@
 function get_data() {
     return {
         "enclosureName": [
-            "矢倉囲い", "角囲い", "Yagura Gakoi", "Kaku Gakoi"
+            "左美濃囲い", "左美濃囲い", "Hidari Mino Gakoi", "Hidari Mino Gakoi"
         ],
         "board": [
             ["sq96", "pc_0"],
             ["sq76", "pc_0"],
-            ["sq66", "pc_0"],
+            ["sq56", "pc_0"],
             ["sq87", "pc_0"],
-            ["sq77", "pc_s"],
+            ["sq77", "pc_0"],
             ["sq67", "pc_0"],
-            ["sq57", "pc_0"],
             ["sq88", "pc_k"],
-            ["sq78", "pc_g"],
+            ["sq78", "pc_s"],
+            ["sq58", "pc_g"],
             ["sq99", "pc_0"],
-            ["sq89", "pc_0"]
+            ["sq89", "pc_0"],
+            ["sq69", "pc_g"]
         ],
         "arrow": [
             ["w88", "k51"],
-            ["h78", "a73"],
-            ["c77", "a1"]
+            ["d78", "a48"],
+            ["d68", "a62"],
+            ["c58", "a1"]
         ]
     };
 }
