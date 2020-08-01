001f5399ccbb0ff6d05e2b5e8efcf3ef67d1addf
むずでょ <muzudho1@gmail.com>
2020-08-01 09:08:52 +0900

    board

--- tool/board.py
+++ tool/board.py
@@ -1,4 +1,23 @@
-for file in reversed(range(1, 10)):
-    for rank in range(1, 10):
+# 盤
+for rank in range(1, 10):
+    for file in reversed(range(1, 10)):
         print(f"""<img id="sq{file}{rank}">""", end="")
+    print(f"""<br>""")
+
+# ヨコ
+for rank in range(1, 10):
+    for file in reversed(range(2, 10)):
+        print(f"""<img id="w{file}{rank}">""", end="")
+    print(f"""<br>""")
+
+# タテ
+for rank in range(2, 10):
+    for file in reversed(range(1, 10)):
+        print(f"""<img id="h{file}{rank}">""", end="")
+    print(f"""<br>""")
+
+# ナナメ
+for rank in range(2, 10):
+    for file in reversed(range(2, 10)):
+        print(f"""<img id="d{file}{rank}">""", end="")
     print(f"""<br>""")
