7491f5618e1e1ca149cb1468669754a1795feee2
むずでょ <muzudho1@gmail.com>
2020-08-02 23:12:08 +0900

    Chudan gyoku

--- data/input-chudan-gyoku-yondan-hashigyoku-gata.js
+++ data/input-chudan-gyoku-yondan-hashigyoku-gata.js
@@ -1,16 +1,16 @@
 function get_data() {
     return {
         "enclosureName": [
-            "中段玉", "空中楼閣型", "Chudan Gyoku", "Kuchu Roukaku Gata"
+            "中段玉", "四段端玉型", "Chudan Gyoku", "Yondan Hashigyoku Gata"
         ],
         "board": [
             ".........",
             ".........",
             ".........",
             ".........",
-            "000......",
-            ".KS......",
-            ".G0......",
+            "0000.....",
+            "KGGS.....",
+            ". 0......",
             ".........",
             "0........",
         ]
