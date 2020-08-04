b0a0e4eb98903c9872e11874d624bf713f29d529
むずでょ <muzudho1@gmail.com>
2020-08-04 18:51:06 +0900

    data

---
+++ data/input-kikusui-yagura.js
@@ -0,0 +1,28 @@
+function get_data() {
+    return {
+        "english": "Chrysanthemums on the water from Wooden observation",
+        "category": [
+            ["矢", "Ya"],
+            ["倉", "g(k)ura"],
+            ["囲", "G(k)ako"],
+            ["い", "i"]
+        ],
+        "formalName": [
+            ["菊", "Kiku"],
+            ["水", "sui"],
+            ["矢", "Ya"],
+            ["倉", "g(k)ura"]
+        ],
+        "board": [
+            ".........",
+            ".........",
+            ".........",
+            ".........",
+            ".........",
+            "0.000....",
+            ".00G.....",
+            ".SG0.....",
+            "0K.......",
+        ]
+    };
+}
