49bf7a6ac40adbc68c2d1f6df069b1a098bf4fa5
むずでょ <muzudho1@gmail.com>
2020-08-07 00:54:47 +0900

    data

---
+++ data/hidari-ginkan-anaguma-1.js
@@ -0,0 +1,34 @@
+function get_data() {
+    return {
+        "link": [
+            "https://shougi.gopoq.net/%e5%b0%86%e6%a3%8b%e3%82%a6%e3%82%a9%e3%83%bc%e3%82%ba%e3%81%ae%e5%9b%b2%e3%81%84%e3%82%92%e5%85%a8%e7%a8%ae%e9%a1%9e%e3%81%be%e3%81%a8%e3%82%81%e3%81%be%e3%81%97%e3%81%9f%e3%80%82/"
+        ],
+        "english": "Silver crown on the left Bear entered a nest type 1",
+        "category": [
+            ["左", "Hidari"],
+            ["美", "Mi"],
+            ["濃", "no"],
+            ["囲", "G(k)ako"],
+            ["い", "i"]
+        ],
+        "formalName": [
+            ["左", "Hidari"],
+            ["銀", "Gin"],
+            ["冠", "kan"],
+            ["穴", "Ana"],
+            ["熊", "g(k)uma"],
+            ["１", "Ichi"]
+        ],
+        "board": [
+            ".........",
+            ".........",
+            ".........",
+            ".........",
+            ".........",
+            ".000.....",
+            "0S0.0....",
+            "0.G.G....",
+            "K0.......",
+        ]
+    };
+}
