b43476a75179df3056e017653b48f83de8b668be
むずでょ <muzudho1@gmail.com>
2020-08-07 00:21:39 +0900

    data

--- data/yonmai-mino-1.js
+++ data/yonmai-mino-1.js
@@ -1,6 +1,9 @@
 function get_data() {
     return {
-        "english": "4 pieces in Mino country",
+        "link": [
+            "https://shougi.gopoq.net/%e5%b0%86%e6%a3%8b%e3%82%a6%e3%82%a9%e3%83%bc%e3%82%ba%e3%81%ae%e5%9b%b2%e3%81%84%e3%82%92%e5%85%a8%e7%a8%ae%e9%a1%9e%e3%81%be%e3%81%a8%e3%82%81%e3%81%be%e3%81%97%e3%81%9f%e3%80%82/"
+        ],
+        "english": "4 pieces in Mino country type 1",
         "category": [
             ["左", "Hidari"],
             ["美", "Mi"],
@@ -12,7 +15,8 @@ function get_data() {
             ["四", "Yon"],
             ["枚", "mai"],
             ["美", "Mi"],
-            ["濃", "no"]
+            ["濃", "no"],
+            ["１", "Ichi"]
         ],
         "board": [
             ".........",
@@ -21,8 +25,8 @@ function get_data() {
             ".........",
             ".........",
             "00000....",
-            ".KSG.....",
-            "..S......",
+            ".KS......",
+            "..S.G....",
             "000G.....",
         ]
     };
---
+++ data/yonmai-mino-2.js
@@ -0,0 +1,33 @@
+function get_data() {
+    return {
+        "link": [
+            "https://www.shougi.jp/kakoi/l-mino/yonmai_mino.html"
+        ],
+        "english": "4 pieces in Mino country type 2",
+        "category": [
+            ["左", "Hidari"],
+            ["美", "Mi"],
+            ["濃", "no"],
+            ["囲", "G(k)ako"],
+            ["い", "i"]
+        ],
+        "formalName": [
+            ["四", "Yon"],
+            ["枚", "mai"],
+            ["美", "Mi"],
+            ["濃", "no"],
+            ["２", "Ni"]
+        ],
+        "board": [
+            ".........",
+            ".........",
+            ".........",
+            ".........",
+            ".........",
+            "00000....",
+            ".KSG.....",
+            "..S......",
+            "000G.....",
+        ]
+    };
+}
