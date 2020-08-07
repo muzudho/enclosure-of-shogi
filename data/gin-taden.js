86e427e72a34d0cd914eb980d8784be0e0dd194b
むずでょ <muzudho1@gmail.com>
2020-08-08 03:44:52 +0900

    Data

---
+++ data/gin-taden.js
@@ -0,0 +1,30 @@
+function get_data() {
+    return {
+        "link": [
+            "http://shinolabo.com/kakoi/gin_taden.html"
+        ],
+        "english": "Silver tandem",
+        "category": [
+            ["多", "Ta"],
+            ["伝", "den"],
+            ["囲", "G(k)ako"],
+            ["い", "i"]
+        ],
+        "formalName": [
+            ["銀", "Gin"],
+            ["多", "ta"],
+            ["伝", "den"]
+        ],
+        "board": [
+            ".........",
+            ".........",
+            ".........",
+            ".........",
+            ".....0...",
+            "....0S0.0",
+            ".....S00.",
+            ".....GK..",
+            "........0",
+        ]
+    };
+}
---
+++ data/kin-taden.js
@@ -0,0 +1,30 @@
+function get_data() {
+    return {
+        "link": [
+            "http://shinolabo.com/kakoi/kin_taden.html"
+        ],
+        "english": "Gold tandem",
+        "category": [
+            ["多", "Ta"],
+            ["伝", "den"],
+            ["囲", "G(k)ako"],
+            ["い", "i"]
+        ],
+        "formalName": [
+            ["金", "Kin"],
+            ["多", "ta"],
+            ["伝", "den"]
+        ],
+        "board": [
+            ".........",
+            ".........",
+            ".........",
+            ".........",
+            ".....0...",
+            "....0S0.0",
+            ".....G00.",
+            ".....GK..",
+            "........0",
+        ]
+    };
+}
